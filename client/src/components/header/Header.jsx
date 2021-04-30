a2878a4b800ed7d3c66943b09cf1d448872b5942
rllstpwz <rllstpwz@gmail.com>
2021-04-30 14:19:18 +0300

    flags

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,67 +1,81 @@
-import React, {useState, useContext} from 'react'
-import {LanguageContext} from '../../Context/LanguageContext/LanguageContext'
-import './Header.css'
-import { Link } from 'react-router-dom'
-import translate from '../../i18n/translate'
+import React, { useState, useContext } from "react";
+import { LanguageContext } from "../../Context/LanguageContext/LanguageContext";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import translate from "../../i18n/translate";
 
 const Header = () => {
-    const [langCheck, setLangCheck] = useState(false)
-    const [checked, setChecked] = useState(false)
-    //eslint-disable-next-line
-    const [language, setLanguage] = useContext(LanguageContext)
-    const [selectedLanguage, setSelectedLanguage] = useState('https://www.countryflags.io/us/shiny/32.png')
-    //eslint-disable-next-line
-    const handleCheck = () => {
-        setChecked(!checked)
-    }
+  const [langCheck, setLangCheck] = useState(false);
+  const [checked, setChecked] = useState(false);
+  //eslint-disable-next-line
+  const [language, setLanguage] = useContext(LanguageContext);
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    "https://www.countryflags.io/us/flat/32.png"
+  );
+  //eslint-disable-next-line
+  const handleCheck = () => {
+    setChecked(!checked);
+  };
 
-    return (
-        <header className="header">
-            <h1 className='logo'><Link to='/'>{translate('Home')}</Link></h1>
-            
-            <input className="menu-btn" type="checkbox" id="menu-btn" />
-            <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-            <div className='lang-options'>
-                <div className='selected-lang' onClick={() => setLangCheck(!langCheck)}> 
-                    <img className='selected-lang-icon' alt='' src={selectedLanguage}></img>
-                </div>
-                {
-                    langCheck ?
-                        <ul>
-                            <li>
-                                {/* eslint-disable-next-line */}
-                                <a
-                                    className='en'
-                                    onClick={() => {
-                                        setLanguage('en');
-                                        setLangCheck(!langCheck);
-                                        setSelectedLanguage('https://www.countryflags.io/us/shiny/32.png')
-                                    }}>
-                                </a>
-                            </li>
-                            <li>
-                                {/* eslint-disable-next-line */}
-                                <a
-                                    className='bg'
-                                    onClick={() => {
-                                        setLanguage('bg');
-                                        setLangCheck(!langCheck);
-                                        setSelectedLanguage('https://www.countryflags.io/bg/shiny/32.png')
-                                    }}>
+  return (
+    <header className="header">
+      <h1 className="logo">
+        <Link to="/">{translate("Home")}</Link>
+      </h1>
 
-                                </a>
-                            </li>
-                        </ul>
-                        :
-                        null
-                }
-            </div>
-            <ul className="menu">
-                <li><Link to='/tpli'>{translate('L.I.')}</Link></li>
-                <li><Link to='/ci'>{translate('C.I.')}</Link></li>
-            </ul>
-        </header>
-    )
-}
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" htmlFor="menu-btn">
+        <span className="navicon"></span>
+      </label>
+      <div className="lang-options">
+        <div className="selected-lang" onClick={() => setLangCheck(!langCheck)}>
+          <img
+            className="selected-lang-icon"
+            alt=""
+            src={selectedLanguage}
+          ></img>
+        </div>
+        {langCheck ? (
+          <ul>
+            <li>
+              {/* eslint-disable-next-line */}
+              <a
+                className="en"
+                onClick={() => {
+                  setLanguage("en");
+                  setLangCheck(!langCheck);
+                  setSelectedLanguage(
+                    "https://www.countryflags.io/us/flat/32.png"
+                  );
+                }}
+              ></a>
+            </li>
+            <li>
+              {/* eslint-disable-next-line */}
+              <a
+                className="bg"
+                onClick={() => {
+                  setLanguage("bg");
+                  setLangCheck(!langCheck);
+                  setSelectedLanguage(
+                    "https://www.countryflags.io/bg/flat/32.png"
+                  );
+                }}
+              ></a>
+            </li>
+          </ul>
+        ) : null}
+      </div>
+      <ul className="menu">
+        <li>
+          <Link to="/tpli">{translate("L.I.")}</Link>
+        </li>
+        <li>
+          <Link to="/ci">{translate("C.I.")}</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- client/src/components/insuranceAdmin/InsuranceAdmin.jsx
+++ client/src/components/insuranceAdmin/InsuranceAdmin.jsx
@@ -1,183 +1,192 @@
-import { useState, useEffect } from "react"
-import axios from 'axios'
-import { InsuranceClient as Client } from '../insuranceClient/InsuranceClient'
-import './InsuranceAdmin.css'
-import { validateNumberInput } from '../../validation/numbers'
-import { validateLetterInput } from '../../validation/letters'
-import translate from "../../i18n/translate"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { InsuranceClient as Client } from "../insuranceClient/InsuranceClient";
+import "./InsuranceAdmin.css";
+import { validateNumberInput } from "../../validation/numbers";
+import { validateLetterInput } from "../../validation/letters";
+import translate from "../../i18n/translate";
 const InsuranceAdmin = (props) => {
-    let usernames = ['A', 'B', 'C']
-    let password = 'admin1!'
-    let insCompany = ''
+  let usernames = ["A", "B", "C"];
+  let password = "admin1!";
+  let insCompany = "";
 
-    const [queryData, setQueryData] = useState(
-        {
-            name: '',
-            date: '',
-            idn: '',
-            accidentType: ''
-        }
-    )
+  const [queryData, setQueryData] = useState({
+    name: "",
+    date: "",
+    idn: "",
+    accidentType: "",
+  });
 
-    const [clients, setClients] = useState([])
+  const [clients, setClients] = useState([]);
 
-    if (usernames.indexOf(props.username) >= 0 && props.password === password) {
-        insCompany = props.username
+  if (usernames.indexOf(props.username) >= 0 && props.password === password) {
+    insCompany = props.username;
+  }
+  useEffect(() => {
+    axios.get(`http://localhost:3001/${insCompany}`).then((res) => {
+      if (res.status === 200) {
+        setClients(res.data);
+      }
+    });
+
+    //eslint-disable-next-line
+  }, []);
+
+  const handleInput = ({ target }) => {
+    setQueryData({ ...queryData, [target.name]: target.value });
+  };
+
+  const findBy = (criteria) => {
+    switch (criteria) {
+      case "date":
+        axios
+          .post("http://localhost:3001/query/date", {
+            insuranceCompany: insCompany,
+            firstReg: queryData.date,
+          })
+          .then((res) => {
+            if (res.status === 200 && res.data.length === 0) {
+              setClients([]);
+            }
+            if (res.status === 200 && res.data.length !== 0) {
+              setClients(res.data);
+            }
+          });
+        break;
+      case "name":
+        axios
+          .post("http://localhost:3001/query/name", {
+            insuranceCompany: insCompany,
+            name: queryData.name,
+          })
+          .then((res) => {
+            if (res.status === 200 && res.data.length === 0) {
+              setClients([]);
+            }
+            if (res.status === 200 && res.data.length !== 0) {
+              setClients(res.data);
+            }
+          });
+        break;
+      case "idn":
+        console.log(queryData.idn);
+        axios
+          .post("http://localhost:3001/query/idn", {
+            insuranceCompany: insCompany,
+            idn: queryData.idn,
+          })
+          .then((res) => {
+            if (res.status === 200 && res.data.length === 0) {
+              setClients([]);
+            }
+            if (res.status === 200 && res.data.length !== 0) {
+              setClients(res.data);
+            }
+          });
+        break;
+      case "accident":
+        console.log(queryData.accidentType);
+        axios
+          .post("http://localhost:3001/query/accidentType", {
+            insuranceCompany: insCompany,
+            accidentType: queryData.accidentType,
+          })
+          .then((res) => {
+            if (res.status === 200 && res.data.length === 0) {
+              setClients([]);
+            }
+            if (res.status === 200 && res.data.length !== 0) {
+              setClients(res.data);
+            }
+          });
+        break;
+      default:
+        break;
     }
-    useEffect(() => {
-        axios.get(`http://localhost:3001/${insCompany}`)
-            .then(res => {
-                if (res.status === 200) {
-                    setClients(res.data)
-                }
-            })
+  };
+  const payClient = (client) => {
+    axios.post("http://localhost:3001/query/id", { id: client.id });
+    const updatedClients = clients.filter((item) => item.id !== client.id);
+    setClients(updatedClients);
+  };
 
-        //eslint-disable-next-line
-    }, [])
+  return insCompany === "A" || insCompany === "B" || insCompany === "C" ? (
+    <section className="admin-section">
+      <div className="admin-panel">
+        <div className="input-labels">
+          <p>{translate("Find by vehicle inspection expire date:")}</p>
+          <p>{translate("Find by client name:")}</p>
+          <p>{translate("Find by IDN:")}</p>
+          <p>{translate("Find by type of accident:")}</p>
+        </div>
+        <div className="inputs">
+          <input
+            className="form-control"
+            name="date"
+            type="date"
+            onChange={handleInput}
+          />
+          <input
+            className="form-control"
+            name="name"
+            type="text"
+            onKeyPress={validateLetterInput}
+            onChange={handleInput}
+          />
+          <input
+            className="form-control"
+            name="idn"
+            type="text"
+            onKeyPress={validateNumberInput}
+            onChange={handleInput}
+          />
+          <select
+            className="form-control"
+            name="accidentType"
+            onChange={handleInput}
+          >
+            <option value="tpli">T.P.L.I</option>
+            <option value="ra">Road Accident</option>
+            <option value="pd">Parking Damage</option>
+            <option value="nd">Natural Disaster</option>
+            <option value="sc">Self-catastrophic</option>
+          </select>
+        </div>
+        <div className="input-buttons">
+          <button className="btn btn-secondary" onClick={() => findBy("date")}>
+            {translate("FIND")}
+          </button>
+          <button className="btn btn-secondary" onClick={() => findBy("name")}>
+            {translate("FIND")}
+          </button>
+          <button className="btn btn-secondary" onClick={() => findBy("idn")}>
+            {translate("FIND")}
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => findBy("accident")}
+          >
+            {translate("FIND")}
+          </button>
+        </div>
+      </div>
+      {clients.map((item) => (
+        <div>
+          <Client client={item} />
+          <button
+            className="btn btn-secondary mt-2 mb-4"
+            onClick={() => payClient(item)}
+          >
+            {translate("Pay client")}
+          </button>
+        </div>
+      ))}
+    </section>
+  ) : (
+    <section className="error-section">
+      <h1>Incorrect data, refresh the page to try again.</h1>
+    </section>
+  );
+};
 
-    const handleInput = ({ target }) => {
-        setQueryData({ ...queryData, [target.name]: target.value })
-    }
-
-    const findBy = (criteria) => {
-        switch (criteria) {
-            case 'date':
-                axios.post('http://localhost:3001/query/date', {
-                    insuranceCompany: insCompany,
-                    firstReg: queryData.date
-                })
-                    .then(res => {
-                        if(res.status === 200 && res.data.length === 0) {
-                            setClients([])
-                        }
-                        if (res.status === 200 && res.data.length !== 0) {
-                            setClients(res.data)
-                        }
-                    })
-                break;
-            case 'name':
-                axios.post('http://localhost:3001/query/name', {
-                    insuranceCompany: insCompany,
-                    name: queryData.name
-                })
-                    .then(res => {
-                        if(res.status === 200 && res.data.length === 0) {
-                            setClients([])
-                        }
-                        if (res.status === 200 && res.data.length !== 0) {
-                            setClients(res.data)
-                        }
-                    })
-                break;
-            case 'idn':
-                axios.post('http://localhost:3001/query/idn', {
-                    insuranceCompany: insCompany,
-                    idn: queryData.idn
-                })
-                    .then(res => {
-                        if(res.status === 200 && res.data.length === 0) {
-                            setClients([])
-                        }
-                        if (res.status === 200 && res.data.length !== 0) {
-                            setClients(res.data)
-                        }
-                    })
-                break;
-            case 'accident':
-                console.log(queryData.accidentType)
-                axios.post('http://localhost:3001/query/accidentType', {
-                    insuranceCompany: insCompany,
-                    accidentType: queryData.accidentType
-                })
-                    .then(res => {
-                        if(res.status === 200 && res.data.length === 0) {
-                            setClients([])
-                        } 
-                        if (res.status === 200 && res.data.length !== 0) {
-                            setClients(res.data)
-                        }
-                    })
-                break;
-            default:
-                break;
-        }
-    }
-    const payClient = (client) => {
-        axios.post('http://localhost:3001/query/id', { id: client.id })
-        const updatedClients = clients.filter(item => item.id !== client.id)
-        setClients(updatedClients)
-    }
-
-
-
-    return (
-        insCompany === 'A' || insCompany === 'B' || insCompany === 'C' ?
-            <section className='admin-section'>
-                <div className='admin-panel'>
-                    <div className='input-labels'>
-                        <p>{translate('Find by vehicle inspection expire date:')}</p>
-                        <p>{translate('Find by client name:')}</p>
-                        <p>{translate('Find by IDN:')}</p>
-                        <p>{translate('Find by type of accident:')}</p>
-                    </div>
-                    <div className='inputs'>
-                        <input
-                            className="form-control"
-                            name='date'
-                            type="date"
-                            onChange={handleInput}
-                        />
-                        <input
-                            className="form-control"
-                            name='name'
-                            type="text"
-                            onKeyPress={validateLetterInput}
-                            onChange={handleInput}
-                        />
-                        <input
-                            className="form-control"
-                            name='idn'
-                            type="text"
-                            onKeyPress={validateNumberInput}
-                            onChange={handleInput}
-                        />
-                        <select
-                            className="form-control"
-                            name='accidentType'
-                            onChange={handleInput}
-                        >
-                            <option value='tpli'>T.P.L.I</option>
-                            <option value='ra'>Road Accident</option>
-                            <option value='pd'>Parking Damage</option>
-                            <option value='nd'>Natural Disaster</option>
-                            <option value='sc'>Self-catastrophic</option>
-                        </select>
-                    </div>
-                    <div className='input-buttons'>
-                        <button className='btn btn-secondary' onClick={() => findBy('date')}>{translate('FIND')}</button>
-                        <button className='btn btn-secondary' onClick={() => findBy('name')}>{translate('FIND')}</button>
-                        <button className='btn btn-secondary' onClick={() => findBy('idn')}>{translate('FIND')}</button>
-                        <button className='btn btn-secondary' onClick={() => findBy('accident')}>{translate('FIND')}</button>
-
-                    </div>
-                </div>
-                {
-
-                    clients.map(item => (
-                        <div>
-                            <Client client={item} />
-                            <button className='btn btn-secondary mt-2 mb-4' onClick={() => payClient(item)}>{translate('Pay client')}</button>
-                        </div>
-                    ))
-
-                }
-
-            </section>
-            :
-            <section className='error-section'>
-                <h1>Incorrect data, refresh the page to try again.</h1>
-            </section>
-    )
-}
-
-export default InsuranceAdmin
+export default InsuranceAdmin;
